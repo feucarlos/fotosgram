aa069359e3762724a907bdb78a1ad982d1df960f
José Carlos Gutiérez Rojas <feucalos@gmail.com>
2020-12-15 16:04:09 -0600

    Obtener posts

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  constructor(private postService: PostsService) {}
+
+  ngOnInit(){
+
+    this.postService.getPosts().subscribe( post => {
+        console.log(post);
+    });
+
+  }
 
 }
---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const URL = environment.url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  paginaPosts = 0;
+
+  constructor(private http: HttpClient) { }
+
+
+  getPosts(){
+    this.paginaPosts++;
+    return this.http.get(`${ URL }/posts?pagina=${ this.paginaPosts }`);
+  }
+}
