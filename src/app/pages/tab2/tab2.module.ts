6c0c5a72d72ea006cf060f80bb6781348e473347
José Carlos Gutiérez Rojas <feucalos@gmail.com>
2020-12-30 19:59:25 -0600

    domSanitizer para IOS

--- src/app/pages/tab2/tab2.module.ts
+++ src/app/pages/tab2/tab2.module.ts
@@ -7,6 +7,7 @@ import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { PipesModule } from 'src/app/pipes/pipes.module';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule, 
+    PipesModule
   ],
   declarations: [Tab2Page]
 })
--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { DomSanitizerPipe } from './dom-sanitizer.pipe';
+import { ImageSanitizerPipe } from './image-sanitizer.pipe';
 
 
 @NgModule({
-  declarations: [DomSanitizerPipe],
+  declarations: [DomSanitizerPipe, ImageSanitizerPipe],
   exports: [
-    DomSanitizerPipe
+    DomSanitizerPipe, ImageSanitizerPipe
   ]
 })
 export class PipesModule { }
